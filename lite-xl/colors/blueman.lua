5608ef8a32fb16b176242c62bdeb0176c570332e
archusXIV <89104690+archusXIV@users.noreply.github.com>
2022-05-31 15:19:44 +0200

    Add blueman theme

---
+++ lite-xl/colors/blueman.lua
@@ -0,0 +1,32 @@
+local style = require "core.style"
+local common = require "core.common"
+
+style.background = { common.color "#1E1F29" }
+style.background2 = { common.color "#252633" } -- 1E1F29
+style.background3 = { common.color "#52546B" }
+style.text = { common.color "#7b81a6" }
+style.caret = { common.color "#f8f8f0" }
+style.accent = { common.color "#8be9fd" }
+style.dim = { common.color "#4f5873" }
+style.divider = { common.color "#1f2029" }
+style.selection = { common.color "#44475a" }
+style.line_number = { common.color "#53576e" }
+style.line_number2 = { common.color "#82AEEF" }
+style.line_highlight = { common.color "#1E1F29" }
+style.scrollbar = { common.color "#44475a" }
+style.scrollbar2 = { common.color "#3060A6" }
+style.guide = { common.color "#44475a" } -- indentguide
+
+style.syntax["normal"] = { common.color "#8EA4C6" }
+style.syntax["symbol"] = { common.color "#4578C6" }
+style.syntax["comment"] = { common.color "#5c6370" }
+style.syntax["keyword"] = { common.color "#9564C7" }
+style.syntax["keyword2"] = { common.color "#009FF0" }
+style.syntax["number"] = { common.color "#82AEEF" }
+style.syntax["literal"] = { common.color "#8be9fd" }
+style.syntax["string"] = { common.color "#3C69A8" }
+style.syntax["string2"] = { common.color "#3C98A8" }
+style.syntax["string3"] = { common.color "#413CA8" }
+style.syntax["operator"] = { common.color "#0060A6" }
+style.syntax["function"] = { common.color "#8970BF" }
+style.syntax["preprocessor"] = { common.color "##7672E8" } -- thinking ahead
