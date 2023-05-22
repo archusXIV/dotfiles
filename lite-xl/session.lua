9a78cb4a016a9a41714f3b62214ca6aacf4f1d35
archusXIV <89104690+archusXIV@users.noreply.github.com>
2023-05-22 10:39:26 +0200

    Delete session.lua

--- lite-xl/session.lua
+++
@@ -1,1 +0,0 @@
-return {recents={[1]="/home/archangel/.local/bin/xtreamripper",[2]="/home/archangel/.local/bin"}, window={[1]=1908,[2]=1044,[3]=1925,[4]=29,["n"]=4}, window_mode="normal", previous_find={}, previous_replace={}}
