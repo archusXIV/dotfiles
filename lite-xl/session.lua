0ecb575dbe224ce9113b3181e102d7e5142f9453
archusXIV <89104690+archusXIV@users.noreply.github.com>
2024-06-18 07:10:49 +0000

    Delete lite-xl directory

--- lite-xl/session.lua
+++
@@ -1,1 +0,0 @@
-return {recents={[1]="/home/archangel/.local/bin/mpv-playlists-manager"}, window={[1]=1906,[2]=1040,[3]=3846,[4]=32,["n"]=4}, window_mode="normal", previous_find={[1]="string2"}, previous_replace={}}
